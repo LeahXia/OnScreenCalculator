0cdf422740562bf1de4e10728e4d650ef7ca106d
Leah Xia <leahmotta@live.com>
2018-03-23 10:42:42 -0700

    show current input after press operators

--- js/script.js
+++ js/script.js
@@ -1,59 +1,56 @@
 $(document).ready(function(){
-    
+
     var inputs="";
     var inputNum;
     var inputNumArr=[];
     var operaterInp=[];
     var results=0;
-    
+    var operaterPressed=false;
+
     $(".numbers").click(function(){
-        
-        document.getElementById("screen").innerHTML+=  $(this).val();
+        if (operaterPressed) document.getElementById("screen").innerHTML = "";
+        document.getElementById("screen").innerHTML +=  $(this).val();
         inputs += $(this).val();
         inputNum = parseFloat(inputs);
-        if(inputNum>999999999){
-        alert("The number you put is too big.")
-        }
+        if(inputNum>999999999) alert("The number you put is too big.")
     });
-    
-    
-        
+
     $("#add").click(function(){
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        document.getElementById("screen").innerHTML="";
         operaterInp.push("add");
         inputs="";
         console.log(inputNumArr);
     });
-    
+
     $("#minus").click(function(){
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        document.getElementById("screen").innerHTML="";
         operaterInp.push("minus");
         inputs="";
         console.log(inputNumArr);
     });
-    
+
     $("#times").click(function(){
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        document.getElementById("screen").innerHTML="";
         operaterInp.push("times");
         inputs="";
     });
-    
+
     $("#divide").click(function(){
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        document.getElementById("screen").innerHTML="";
         operaterInp.push("divide");
         inputs="";
     });
-    
+
     $("#equalTo").click(function(){
         inputNumArr.push(inputNum);
         results = inputNumArr[0];
         var i=0;
         while (i<operaterInp.length){
-        
+
         switch(operaterInp[i]){
             case "add":
                 results += inputNumArr[i+1];
@@ -67,14 +64,14 @@ $(document).ready(function(){
             case "divide":
                 results /= inputNumArr[i+1];
                 break;
-         }  
+         }
             i++;
         }
         document.getElementById("screen").innerHTML= results;
         inputNumArr=[];
         inputs="";
     });
-    
+
     $("#AC").click(function(){
        inputNumArr=[];
        inputs="";
@@ -82,14 +79,14 @@ $(document).ready(function(){
        results=0;
        document.getElementById("screen").innerHTML="";
     });
-    
+
     $(".percent").click(function(){
-      
-      document.getElementById("screen").innerHTML = inputNum / 100; 
+
+      document.getElementById("screen").innerHTML = inputNum / 100;
     });
     $("#L").click(function(){
-      
-      document.getElementById("screen").innerHTML = "BY LEAH"; 
+
+      document.getElementById("screen").innerHTML = "BY LEAH";
     });
-    
+
 });
