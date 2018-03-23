0de14613b936ddadddb8374ea5fa8590397389d4
Leah Xia <leahmotta@live.com>
2018-03-23 14:21:58 -0700

    Revert "Merge pull request #1 from LeahXia/fix-bug-branch"
    This reverts commit 617aff84bea04400d2959e4992af447875d9f1e7, reversing
    changes made to d3489865f32671704b4aa63224ec137d0c640621.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,20 @@
 $(document).ready(function(){
-
+    
+    var inputs="";
     var inputNum;
     var inputNumArr=[];
     var operaterInp=[];
     var results=0;
-    var operaterPressed=false;
-    var didClearScreen=false;
-
+    
     $(".numbers").click(function(){
-        var screenValue = document.getElementById("screen")
-        if (operaterPressed && !didClearScreen) {
-          screenValue.innerHTML = "";
-          operaterPressed = false;
-          didClearScreen = true;
-        }
-        screenValue.innerHTML +=  $(this).val();
-        var inputs = screenValue.innerHTML;
+        
+        document.getElementById("screen").innerHTML+=  $(this).val();
+        inputs += $(this).val();
         inputNum = parseFloat(inputs);
-        if(inputNum>999999999) alert("The number you put is too big.");
+        if(inputNum>999999999){
+        alert("The number you put is too big.")
+        }
     });
-<<<<<<< HEAD
-
-    $(".operater").click(function(){
-        operaterPressed = true;
-        didClearScreen=false;
-=======
     
     
         
@@ -53,54 +43,54 @@ $(document).ready(function(){
     });
     
     $("#divide").click(function(){
->>>>>>> d3489865f32671704b4aa63224ec137d0c640621
         inputNumArr.push(inputNum);
-        var opValue = $(this).val();
-        operaterInp.push(opValue);
-    })
-
+        document.getElementById("screen").innerHTML="";
+        operaterInp.push("divide");
+        inputs="";
+    });
+    
     $("#equalTo").click(function(){
-        operaterPressed=true;
-        didClearScreen=false;
         inputNumArr.push(inputNum);
         results = inputNumArr[0];
         var i=0;
         while (i<operaterInp.length){
-            switch(operaterInp[i]){
-                case "add":
-                    results += inputNumArr[i+1];
-                    break;
-                case "minus":
-                    results -= inputNumArr[i+1];
-                    break;
-                case "times":
-                    results *= inputNumArr[i+1];
-                    break;
-                case "divide":
-                    results /= inputNumArr[i+1];
-                    break;
-             }
-                i++;
+        
+        switch(operaterInp[i]){
+            case "add":
+                results += inputNumArr[i+1];
+                break;
+            case "minus":
+                results -= inputNumArr[i+1];
+                break;
+            case "times":
+                results *= inputNumArr[i+1];
+                break;
+            case "divide":
+                results /= inputNumArr[i+1];
+                break;
+         }  
+            i++;
         }
-        results = results.toFixed(6);
         document.getElementById("screen").innerHTML= results;
         inputNumArr=[];
-        operaterInp=[];
-        inputNum = results
+        inputs="";
     });
-
+    
     $("#AC").click(function(){
        inputNumArr=[];
+       inputs="";
        operaterInp=[];
        results=0;
        document.getElementById("screen").innerHTML="";
     });
-
+    
     $(".percent").click(function(){
-      document.getElementById("screen").innerHTML = inputNum / 100;
+      
+      document.getElementById("screen").innerHTML = inputNum / 100; 
     });
     $("#L").click(function(){
-      document.getElementById("screen").innerHTML = "BY LEAH";
+      
+      document.getElementById("screen").innerHTML = "BY LEAH"; 
     });
-
+    
 });
