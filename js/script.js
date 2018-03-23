41d79c3fc004748ed9a690ab7f7648c80ad3ed3f
Leah Xia <leahmotta@live.com>
2018-03-23 14:40:04 -0700

    Revert "fix operation right after equal pressed; display result with 6 digit after decimal"
    This reverts commit c5f0b34d59b9d7a42018cae958790b4dddf646c2.

--- js/script.js
+++ js/script.js
@@ -1,105 +1,91 @@
 $(document).ready(function(){
 
+    var inputs="";
     var inputNum;
     var inputNumArr=[];
     var operaterInp=[];
     var results=0;
     var operaterPressed=false;
-    var didClearScreen=false;
 
     $(".numbers").click(function(){
-        var screenValue = document.getElementById("screen")
-        if (operaterPressed && !didClearScreen) {
-          screenValue.innerHTML = "";
-          operaterPressed = false;
-          didClearScreen = true;
-        }
-        screenValue.innerHTML +=  $(this).val();
-        var inputs = screenValue.innerHTML;
+        if (operaterPressed) document.getElementById("screen").innerHTML = "";
+        document.getElementById("screen").innerHTML +=  $(this).val();
+        inputs += $(this).val();
         inputNum = parseFloat(inputs);
-        if(inputNum>999999999) alert("The number you put is too big.");
+        if(inputNum>999999999) alert("The number you put is too big.")
     });
-<<<<<<< HEAD
 
-    $(".operater").click(function(){
-        operaterPressed = true;
-        didClearScreen=false;
-=======
-    
-    
-        
     $("#add").click(function(){
-        document.getElementById("screen").innerHTML= "123";
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        document.getElementById("screen").innerHTML="";
         operaterInp.push("add");
         inputs="";
         console.log(inputNumArr);
     });
-    
+
     $("#minus").click(function(){
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        document.getElementById("screen").innerHTML="";
         operaterInp.push("minus");
         inputs="";
         console.log(inputNumArr);
     });
-    
+
     $("#times").click(function(){
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        document.getElementById("screen").innerHTML="";
         operaterInp.push("times");
         inputs="";
     });
-    
+
     $("#divide").click(function(){
->>>>>>> d3489865f32671704b4aa63224ec137d0c640621
+        operaterPressed = true;
         inputNumArr.push(inputNum);
-        var opValue = $(this).val();
-        operaterInp.push(opValue);
-    })
+        operaterInp.push("divide");
+        inputs="";
+    });
 
     $("#equalTo").click(function(){
-        operaterPressed=true;
-        didClearScreen=false;
         inputNumArr.push(inputNum);
         results = inputNumArr[0];
         var i=0;
         while (i<operaterInp.length){
-            switch(operaterInp[i]){
-                case "add":
-                    results += inputNumArr[i+1];
-                    break;
-                case "minus":
-                    results -= inputNumArr[i+1];
-                    break;
-                case "times":
-                    results *= inputNumArr[i+1];
-                    break;
-                case "divide":
-                    results /= inputNumArr[i+1];
-                    break;
-             }
-                i++;
+
+        switch(operaterInp[i]){
+            case "add":
+                results += inputNumArr[i+1];
+                break;
+            case "minus":
+                results -= inputNumArr[i+1];
+                break;
+            case "times":
+                results *= inputNumArr[i+1];
+                break;
+            case "divide":
+                results /= inputNumArr[i+1];
+                break;
+         }
+            i++;
         }
-        results = results.toFixed(6);
         document.getElementById("screen").innerHTML= results;
         inputNumArr=[];
-        operaterInp=[];
-        inputNum = results
+        inputs="";
     });
 
     $("#AC").click(function(){
        inputNumArr=[];
+       inputs="";
        operaterInp=[];
        results=0;
        document.getElementById("screen").innerHTML="";
     });
 
     $(".percent").click(function(){
+
       document.getElementById("screen").innerHTML = inputNum / 100;
     });
     $("#L").click(function(){
+
       document.getElementById("screen").innerHTML = "BY LEAH";
     });
 
